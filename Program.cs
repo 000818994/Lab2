e38a32a1f7b21a0bfa59b16f51931357634f4c98
000818994 <115376403+000818994@users.noreply.github.com>
2022-10-08 17:47:20 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+using Fall_2022_Lab_1_000818994.Data;
+using Fall_2022_Lab_1_000818994.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fall_2022_Lab_1_000818994 {
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                   .AddEntityFrameworkStores<ApplicationDbContext>()
+                   .AddDefaultTokenProviders()
+                   .AddDefaultUI();
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            var configuration = app.Services.GetService<IConfiguration>();
+            var hosting = app.Services.GetService<IWebHostEnvironment>();
+
+            //if (hosting.IsDevelopment())
+            //{
+            //    var secrets = configuration.GetSection("Secrets").Get<AppSecrets>();
+            //    DbInitializer.appSecrets = secrets;
+            //}
+
+            //Seed User Data
+           using (var scope = app.Services.CreateScope())
+           {
+              DbInitializer.SeedUsersAndRoles(scope.ServiceProvider).Wait();
+           }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+   
+}
